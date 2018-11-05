4a3fc20f48034b4095f615eb85514febb6295815
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-11-06 07:01:48 +0900

    CanvasMap: 地図を指定可能に

--- src/main/java/mirrg/minecraft/regioneditor/gui/CanvasMap.java
+++ src/main/java/mirrg/minecraft/regioneditor/gui/CanvasMap.java
@@ -1,8 +1,24 @@
 package mirrg.minecraft.regioneditor.gui;
 
 import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 public class CanvasMap extends Canvas
 {
+
+	private BufferedImage map = null;
+
+	public void setMap(BufferedImage map)
+	{
+		this.map = map;
+		repaint();
+	}
+
+	@Override
+	public void paint(Graphics g)
+	{
+		if (map != null) g.drawImage(map, 0, 0, null);
+	}
 
 }
