32875879cbb952cf38be2b140136ce8319ac2401
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-11-06 00:52:20 +0900

    add: MainRegionEditor

---
+++ src/main/java/mirrg/minecraft/regioneditor/MainRegionEditor.java
@@ -0,0 +1,13 @@
+package mirrg.minecraft.regioneditor;
+
+import mirrg.minecraft.regioneditor.gui.FrameRegionEditor;
+
+public class MainRegionEditor
+{
+
+	public static void main(String[] args)
+	{
+		new FrameRegionEditor().show();
+	}
+
+}
