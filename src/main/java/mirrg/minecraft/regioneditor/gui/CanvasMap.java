4ffb6866665a338fceaa0567bd7f37ad40a1c4d3
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-11-07 04:19:00 +0900

    CanvasMap: has x, z

--- src/main/java/mirrg/minecraft/regioneditor/gui/CanvasMap.java
+++ src/main/java/mirrg/minecraft/regioneditor/gui/CanvasMap.java
@@ -8,6 +8,8 @@ public class CanvasMap extends Canvas
 {
 
 	private BufferedImage map = null;
+	private int x = 0;
+	private int z = 0;
 
 	public void setMap(BufferedImage map)
 	{
@@ -18,7 +20,24 @@ public class CanvasMap extends Canvas
 	@Override
 	public void paint(Graphics g)
 	{
-		if (map != null) g.drawImage(map, 0, 0, null);
+		if (map != null) g.drawImage(map, 0 - x * 16, 0 - z * 16, null);
+	}
+
+	public int getPositionX()
+	{
+		return x;
+	}
+
+	public int getPositionZ()
+	{
+		return z;
+	}
+
+	public void setPosition(int x, int z)
+	{
+		this.x = x;
+		this.z = z;
+		repaint();
 	}
 
 }
