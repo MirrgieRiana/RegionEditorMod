832e6b0122916aa2d15ecd78fc0ff8460c4114e0
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-11-08 01:10:49 +0900

    CanvasMap: x, z -> positionX, positionZ

--- src/main/java/mirrg/minecraft/regioneditor/gui/CanvasMap.java
+++ src/main/java/mirrg/minecraft/regioneditor/gui/CanvasMap.java
@@ -8,8 +8,8 @@ public class CanvasMap extends Canvas
 {
 
 	private BufferedImage map = null;
-	private int x = 0;
-	private int z = 0;
+	private int positionX = 0;
+	private int positionZ = 0;
 
 	public void setMap(BufferedImage map)
 	{
@@ -20,23 +20,23 @@ public class CanvasMap extends Canvas
 	@Override
 	public void paint(Graphics g)
 	{
-		if (map != null) g.drawImage(map, 0 - x * 16, 0 - z * 16, null);
+		if (map != null) g.drawImage(map, 0 - positionX * 16, 0 - positionZ * 16, null);
 	}
 
 	public int getPositionX()
 	{
-		return x;
+		return positionX;
 	}
 
 	public int getPositionZ()
 	{
-		return z;
+		return positionZ;
 	}
 
 	public void setPosition(int x, int z)
 	{
-		this.x = x;
-		this.z = z;
+		this.positionX = x;
+		this.positionZ = z;
 		repaint();
 	}
 
