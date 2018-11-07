922cfed7cfd2274298102d5f87d15990680523b7
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-11-08 02:53:21 +0900

    FontRenderer: 色付き描画サポート

--- src/main/java/mirrg/minecraft/regioneditor/gui/FontRenderer.java
+++ src/main/java/mirrg/minecraft/regioneditor/gui/FontRenderer.java
@@ -1,6 +1,6 @@
 package mirrg.minecraft.regioneditor.gui;
 
-import java.awt.Graphics;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -26,7 +26,7 @@ public class FontRenderer
 	/**
 	 * 指定座標を左上として描画する。
 	 */
-	public static void drawCharacter(Graphics g, char ch, int x, int y)
+	public static void drawCharacter(BufferedImage image, char ch, int x, int y, Color color)
 	{
 		int sx;
 		int sy;
@@ -38,30 +38,52 @@ public class FontRenderer
 			sy = 0;
 		}
 
-		g.drawImage(
-			imageFont,
-			x,
-			y,
-			x + FONT_WIDTH,
-			y + FONT_HEIGHT,
-			sx * FONT_WIDTH,
-			sy * FONT_HEIGHT,
-			sx * FONT_WIDTH + FONT_WIDTH,
-			sy * FONT_HEIGHT + FONT_HEIGHT,
-			null);
+		int br = color.getRed();
+		int bg = color.getGreen();
+		int bb = color.getBlue();
+
+		for (int xi = 0; xi < FONT_WIDTH; xi++) {
+			for (int yi = 0; yi < FONT_HEIGHT; yi++) {
+				if (x + xi < 0) continue;
+				if (x + xi >= image.getWidth()) continue;
+				if (y + yi < 0) continue;
+				if (y + yi >= image.getHeight()) continue;
+
+				int s = imageFont.getRGB(sx * FONT_WIDTH + xi, sy * FONT_HEIGHT + yi);
+				int d = image.getRGB(x + xi, y + yi);
+
+				int sa = (s >> 24) & 0xff;
+				int sr = ((s >> 16) & 0xff) * br / 255;
+				int sg = ((s >> 8) & 0xff) * bg / 255;
+				int sb = ((s >> 0) & 0xff) * bb / 255;
+				int dr = (d >> 16) & 0xff;
+				int dg = (d >> 8) & 0xff;
+				int db = (d >> 0) & 0xff;
+
+				int cr = (sr * sa + dr * (255 - sa)) / 255;
+				int cg = (sg * sa + dg * (255 - sa)) / 255;
+				int cb = (sb * sa + db * (255 - sa)) / 255;
+
+				int c = (cr << 16) | (cg << 8) | (cb << 0);
+
+				image.setRGB(x + xi, y + yi, c);
+			}
+		}
+
 	}
 
 	/**
 	 * 指定座標を上端中心として描画する。
 	 */
-	public static void drawString(Graphics g, String string, int x, int y)
+	public static void drawString(BufferedImage image, String string, int x, int y, Color color)
 	{
 		for (int i = 0; i < string.length(); i++) {
 			drawCharacter(
-				g,
+				image,
 				string.charAt(i),
 				x - FONT_WIDTH / 2 - OFFSET_X / 2 * (string.length() - 1) + i * OFFSET_X,
-				y);
+				y,
+				color);
 		}
 	}
 
