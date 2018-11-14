123fdba790ff3cad4f24fed44b88e7b1d2d9258e
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-11-15 00:46:13 +0900

    add: RegionEntry

---
+++ src/main/java/mirrg/minecraft/regioneditor/data/RegionEntry.java
@@ -0,0 +1,25 @@
+package mirrg.minecraft.regioneditor.data;
+
+public final class RegionEntry
+{
+
+	public final RegionIdentifier regionIdentifier;
+	public final RegionInfo regionInfo;
+
+	public RegionEntry(RegionIdentifier regionIdentifier, RegionInfo regionInfo)
+	{
+		this.regionIdentifier = regionIdentifier;
+		this.regionInfo = regionInfo;
+	}
+
+	@Override
+	public String toString()
+	{
+		return String.format("(%s)%s:(%s)%s",
+			regionIdentifier.countryNumber,
+			regionInfo.countryName,
+			regionIdentifier.stateNumber,
+			regionInfo.stateName);
+	}
+
+}
