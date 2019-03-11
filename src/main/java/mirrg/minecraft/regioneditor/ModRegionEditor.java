674e2cac4f8f4c035f85acc5e9f01619895ba364
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-03-11 18:13:06 +0900

    ModRegionEditor: suppress

--- src/main/java/mirrg/minecraft/regioneditor/ModRegionEditor.java
+++ src/main/java/mirrg/minecraft/regioneditor/ModRegionEditor.java
@@ -26,6 +26,7 @@ public class ModRegionEditor
 	public static final String NAME = "RegionEditor";
 	public static final String VERSION = "0.0.1";
 
+	@SuppressWarnings("unused")
 	private static Logger logger;
 
 	@EventHandler
