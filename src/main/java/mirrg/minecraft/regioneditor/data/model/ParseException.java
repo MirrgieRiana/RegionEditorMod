9c8627ae0092e316a1dead30eebdf69f566b4f30
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-04-20 16:35:25 +0900

    add: ParseException

---
+++ src/main/java/mirrg/minecraft/regioneditor/data/model/ParseException.java
@@ -0,0 +1,31 @@
+package mirrg.minecraft.regioneditor.data.model;
+
+public class ParseException extends Exception
+{
+
+	public ParseException()
+	{
+		super();
+	}
+
+	protected ParseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+	{
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ParseException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public ParseException(String message)
+	{
+		super(message);
+	}
+
+	public ParseException(Throwable cause)
+	{
+		super(cause);
+	}
+
+}
