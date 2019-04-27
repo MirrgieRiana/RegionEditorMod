a9a13d5451f2d6df8383dee964eb918619782709
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-04-27 17:48:24 +0900

    GuiData: (use Action; ショトカ作成。ESCで閉じる)

--- src/main/java/mirrg/minecraft/regioneditor/gui/guis/GuiData.java
+++ src/main/java/mirrg/minecraft/regioneditor/gui/guis/GuiData.java
@@ -5,10 +5,16 @@ import static mirrg.minecraft.regioneditor.util.gui.SwingUtils.*;
 import java.awt.CardLayout;
 import java.awt.Dialog.ModalityType;
 import java.awt.Font;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
+import javax.swing.JComponent;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 
 import mirrg.boron.util.i18n.I18n;
+import mirrg.minecraft.regioneditor.util.gui.ActionBuilder;
+import mirrg.minecraft.regioneditor.util.gui.ActionButton;
 import mirrg.minecraft.regioneditor.util.gui.WindowWrapper;
 
 public class GuiData extends GuiBase
@@ -16,19 +22,40 @@ public class GuiData extends GuiBase
 
 	private IDialogDataListener listener;
 
+	private ActionButton actionExport;
+	private ActionButton actionImport;
+	@SuppressWarnings("unused")
+	private ActionButton actionClose;
+
+	private JTextArea textArea;
+
 	public GuiData(WindowWrapper owner, I18n i18n, IDialogDataListener listener)
 	{
 		super(owner, i18n, i18n.localize("GuiData.title"), ModalityType.MODELESS);
 		this.listener = listener;
 	}
 
-	private JTextArea textArea;
-
 	@Override
 	protected void initComponenets()
 	{
-		windowWrapper.getWindow().setLayout(new CardLayout());
 
+		actionExport = new ActionBuilder<>(new ActionButton(e -> {
+			textArea.setText(listener.onExport());
+		}))
+			.keyStroke(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_DOWN_MASK))
+			.register(windowWrapper.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW), windowWrapper.getRootPane().getActionMap());
+		actionImport = new ActionBuilder<>(new ActionButton(e -> {
+			listener.onImport(textArea.getText());
+		}))
+			.keyStroke(KeyStroke.getKeyStroke(KeyEvent.VK_I, InputEvent.CTRL_DOWN_MASK))
+			.register(windowWrapper.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW), windowWrapper.getRootPane().getActionMap());
+		actionClose = new ActionBuilder<>(new ActionButton(e -> {
+			windowWrapper.getWindow().dispose();
+		}))
+			.keyStroke(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0))
+			.register(windowWrapper.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW), windowWrapper.getRootPane().getActionMap());
+
+		windowWrapper.getWindow().setLayout(new CardLayout());
 		windowWrapper.getWindow().add(borderPanelDown(
 
 			scrollPane(textArea = get(new JTextArea(), c -> {
@@ -37,17 +64,14 @@ public class GuiData extends GuiBase
 
 			flowPanel(
 
-				button(localize("GuiData.buttonExport"), e2 -> {
-					textArea.setText(listener.onExport());
-				}),
+				button(localize("GuiData.buttonExport"), actionExport),
 
-				button(localize("GuiData.buttonImport"), e2 -> {
-					listener.onImport(textArea.getText());
-				})
+				button(localize("GuiData.buttonImport"), actionImport)
 
 			)
 
 		));
+
 	}
 
 	public static interface IDialogDataListener
