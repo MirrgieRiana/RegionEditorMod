87e91438332556fa0fc06ceabb619b66476046e2
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-04-29 17:10:19 +0900

    ToolFill: 塗りつぶし予定範囲を表示する

--- src/main/java/mirrg/minecraft/regioneditor/gui/tools/ToolFill.java
+++ src/main/java/mirrg/minecraft/regioneditor/gui/tools/ToolFill.java
@@ -1,8 +1,13 @@
 package mirrg.minecraft.regioneditor.gui.tools;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayDeque;
 import java.util.HashSet;
 import java.util.Optional;
@@ -42,64 +47,7 @@ public class ToolFill extends ToolBase
 			if (!oTileCoordinates.isPresent()) return;
 
 			setTiles(oTileCoordinates.get(), tile);
-		}
-
-		private Optional<Set<TileCoordinate>> calc(TileCoordinate tileCoordinateOrigin)
-		{
-			TileMapModel tileMapModel = toolContext.getLayerController().tileMapController.model;
-			TileRectangle boundingBox = AreaExtractor.getBoundingBox(tileMapModel);
-			Optional<RegionIdentifier> tileOrigin = tileMapModel.getTile(tileCoordinateOrigin);
-
-			return new Object() {
-				private HashSet<TileCoordinate> tileCoordinatesVisited;
-				private ArrayDeque<TileCoordinate> tileCoordinatesWaiting;
-
-				private Optional<Set<TileCoordinate>> calc()
-				{
-					tileCoordinatesVisited = new HashSet<>();
-					tileCoordinatesWaiting = new ArrayDeque<>();
-
-					visit(tileCoordinateOrigin);
-
-					while (!tileCoordinatesWaiting.isEmpty()) {
-						TileCoordinate tileCoordinate = tileCoordinatesWaiting.removeFirst();
-
-						// 四方のマスが踏める（同属性かつ未訪問）場合、そのマスを踏みつつ訪問先に予約する
-						try {
-							tryVisit(tileCoordinate.plus(-1, 0));
-							tryVisit(tileCoordinate.plus(1, 0));
-							tryVisit(tileCoordinate.plus(0, -1));
-							tryVisit(tileCoordinate.plus(0, 1));
-						} catch (BoundingBoxOverflowException e) {
-							return Optional.empty();
-						}
-
-					}
-
-					return Optional.of(tileCoordinatesVisited);
-				}
-
-				private void tryVisit(TileCoordinate tileCoordinate) throws BoundingBoxOverflowException
-				{
-					Optional<RegionIdentifier> tile = tileMapModel.getTile(tileCoordinate);
-
-					if (!tile.equals(tileOrigin)) return; // 原点と同じ属性のマスで、
-					if (!tile.isPresent() && !boundingBox.contains(tileCoordinate)) throw new BoundingBoxOverflowException(); // 範囲が無限ではなく
-					if (tileCoordinatesVisited.contains(tileCoordinate)) return; // かつ訪問済みでなければ
-					visit(tileCoordinate);
-				}
-
-				private void visit(TileCoordinate tileCoordinate)
-				{
-					tileCoordinatesVisited.add(tileCoordinate);
-					tileCoordinatesWaiting.addLast(tileCoordinate);
-				}
-
-				class BoundingBoxOverflowException extends Exception
-				{
-
-				}
-			}.calc();
+			updateCursor(e.getPoint(), true);
 		}
 
 		private void setTiles(Set<TileCoordinate> tileCoordinates, Optional<RegionIdentifier> tile)
@@ -117,12 +65,20 @@ public class ToolFill extends ToolBase
 			}
 		}
 	};
+	private MouseMotionListener mouseMotionListener = new MouseMotionAdapter() {
+		@Override
+		public void mouseMoved(MouseEvent e)
+		{
+			updateCursor(e.getPoint(), false);
+		}
+	};
 
 	@Override
 	public void on()
 	{
 		super.on();
 		toolContext.getComponent().addMouseListener(mouseListener);
+		toolContext.getComponent().addMouseMotionListener(mouseMotionListener);
 	}
 
 	@Override
@@ -130,6 +86,119 @@ public class ToolFill extends ToolBase
 	{
 		super.off();
 		toolContext.getComponent().removeMouseListener(mouseListener);
+		toolContext.getComponent().removeMouseMotionListener(mouseMotionListener);
+	}
+
+	@Override
+	public void draw(Graphics2D graphics)
+	{
+
+		// 塗りつぶし範囲を描画
+		if (oTileCoordinates.isPresent()) {
+
+			// タイルの表示大きさ
+			int size = toolContext.getTileSize();
+
+			for (TileCoordinate tileCoordinate2 : oTileCoordinates.get()) {
+
+				// 可視範囲にあるなら
+				if (toolContext.isVisible(tileCoordinate2)) {
+
+					// 描画
+					Point position = toolContext.getTilePosition(tileCoordinate2);
+
+					graphics.setColor(Color.white);
+					graphics.drawLine(
+						position.x,
+						position.y,
+						position.x + size,
+						position.y + size);
+					graphics.drawLine(
+						position.x + size,
+						position.y,
+						position.x,
+						position.y + size);
+
+				}
+
+			}
+		}
+
+	}
+
+	private Optional<Set<TileCoordinate>> calc(TileCoordinate tileCoordinateOrigin)
+	{
+		TileMapModel tileMapModel = toolContext.getLayerController().tileMapController.model;
+		TileRectangle boundingBox = AreaExtractor.getBoundingBox(tileMapModel);
+		Optional<RegionIdentifier> tileOrigin = tileMapModel.getTile(tileCoordinateOrigin);
+
+		return new Object() {
+			private HashSet<TileCoordinate> tileCoordinatesVisited;
+			private ArrayDeque<TileCoordinate> tileCoordinatesWaiting;
+
+			private Optional<Set<TileCoordinate>> calc()
+			{
+				tileCoordinatesVisited = new HashSet<>();
+				tileCoordinatesWaiting = new ArrayDeque<>();
+
+				visit(tileCoordinateOrigin);
+
+				while (!tileCoordinatesWaiting.isEmpty()) {
+					TileCoordinate tileCoordinate = tileCoordinatesWaiting.removeFirst();
+
+					// 四方のマスが踏める（同属性かつ未訪問）場合、そのマスを踏みつつ訪問先に予約する
+					try {
+						tryVisit(tileCoordinate.plus(-1, 0));
+						tryVisit(tileCoordinate.plus(1, 0));
+						tryVisit(tileCoordinate.plus(0, -1));
+						tryVisit(tileCoordinate.plus(0, 1));
+					} catch (BoundingBoxOverflowException e) {
+						return Optional.empty();
+					}
+
+				}
+
+				return Optional.of(tileCoordinatesVisited);
+			}
+
+			private void tryVisit(TileCoordinate tileCoordinate) throws BoundingBoxOverflowException
+			{
+				Optional<RegionIdentifier> tile = tileMapModel.getTile(tileCoordinate);
+
+				if (!tile.equals(tileOrigin)) return; // 原点と同じ属性のマスで、
+				if (!tile.isPresent() && !boundingBox.contains(tileCoordinate)) throw new BoundingBoxOverflowException(); // 範囲が無限ではなく
+				if (tileCoordinatesVisited.contains(tileCoordinate)) return; // かつ訪問済みでなければ
+				visit(tileCoordinate);
+			}
+
+			private void visit(TileCoordinate tileCoordinate)
+			{
+				tileCoordinatesVisited.add(tileCoordinate);
+				tileCoordinatesWaiting.addLast(tileCoordinate);
+			}
+
+			class BoundingBoxOverflowException extends Exception
+			{
+
+			}
+		}.calc();
+	}
+
+	private Optional<TileCoordinate> oTileCoordinate = Optional.empty();
+	private Optional<Set<TileCoordinate>> oTileCoordinates = Optional.empty();
+
+	private void updateCursor(Point point, boolean forced)
+	{
+
+		// タイル座標
+		TileCoordinate tileCoordinate = toolContext.getTileCoordinate(point);
+
+		// 未更新なら更新
+		if (forced || (!oTileCoordinate.isPresent() || !oTileCoordinate.get().equals(tileCoordinate))) {
+			oTileCoordinate = Optional.of(tileCoordinate);
+			oTileCoordinates = calc(tileCoordinate);
+		}
+
 	}
 
 }
